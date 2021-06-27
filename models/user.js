91208a1789c49f56d04029df1dbcd3bf1c782f67
M4rkFlor <M4rkFlor@users.noreply.github.com>
2021-06-26 22:59:41 -0700

    Update
    oAuth2

--- models/user.js
+++ models/user.js
@@ -4,11 +4,24 @@ var mongoose = require("mongoose");
 var jwt = require('jsonwebtoken');//npm install jsonwebtoken
 let EMAIL_SECRET = process.env.EMAIL_SECRET;
 const nodemailer = require("nodemailer");//Use OAUTH2 or Enable less secure apps
+//const google = require("googleapis");
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET; 
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
+//const REDIRECT_URI = process.env.REDIRECT_URI 
+//const oauth2Client = new google.auth.Oauth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+//oauth2Client.setCredentials({refresh_token: REFRESH_TOKEN});
+//const accToken = await oauth2Client.getAccessToken();
 const transporter = nodemailer.createTransport({
   service: 'Gmail',
   auth: {
+    type: 'OAuth2',
     user: process.env.GMAIL_USER, // replace these with email and email password
-    pass: process.env.GMAIL_PASS  // Also need to dissable less secure apps https://stackoverflow.com/questions/19877246/nodemailer-with-gmail-and-nodejs
+    clientId: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+    refreshToken: REFRESH_TOKEN
+    //accessToken shoudl be automatically generated on failed attempt, claims the documentations
+    //pass: process.env.GMAIL_PASS  // Also need to dissable less secure apps https://stackoverflow.com/questions/19877246/nodemailer-with-gmail-and-nodejs
   },
 });
 var userSchema = mongoose.Schema({
@@ -72,7 +85,7 @@ jwt.sign(
       subject: 'Confirm Email',
       html: `Warning: this service is FRE<a href="${url}" style="color:#222; text-decoration: none;">E</a> as in 0.00$ , do NOT provide a real credit/debit card number at checkout, use FakeNameGenerator or any other tool. Click the link hidden in the last 'E' of FREE, then you can login, Also by creating an account you agree to the terms of service. 
       Tip: Filling the paypal form takes a long time, so I made a chrome extension on <a href="${giturl}" style="color:#222; text-decoration: none;">github</a>. You should dissable the extension when your done using it, or else it will probably annoy you.`,
-    });
+    });//should be in try catch, but if this fails then it wont work anyway.
   },
 );
 
